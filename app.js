f7fe190245007758253e244dbaa46b9b9331511e
wujun4code <wujun19890209@gmail.com>
2018-02-13 16:30:09 +0800

    chore: C# is better.

--- app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var express = require('express');
-var path = require('path');
-var AV = require('leanengine');
-
-var app = express();
-app.use(AV.express());
-
-module.exports = app;
---
+++ src/Slark/Server/Slark.Server.Core/SlackServer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slark.Server.Core
+{
+    public abstract class SlackServer
+    {
+        IConnectionManager ConnectionManager { get; set; }
+    }
+}
